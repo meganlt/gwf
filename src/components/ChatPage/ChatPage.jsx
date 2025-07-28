f1bd527dcdfdab1d17b183211a5fb8962b52583e
Zoe Copoulos <zoe.copoulos@gmail.com>
2025-07-28 11:14:30 -0500

    chat page framework

--- src/components/ChatPage/ChatPage.jsx
+++ src/components/ChatPage/ChatPage.jsx
@@ -26,25 +26,24 @@ function ChatPage() {
   const messages = [
     {
       id: 1,
-      message: "Hover me!",
-      sender: "user",
+      message: 'Hover me!',
+      sender: 'user',
     },
     {
       id: 2,
-      message: "Hover me too!",
-      sender: "bot",
+      message: 'Hover me too!',
+      sender: 'bot',
     },
     {
       id: 3,
-      message: "",
-      sender: "bot",
+      message: '',
+      sender: 'bot',
       isLoading: true,
     },
   ];
 
+  const actionIcons = [];
 
-   const actionIcons = [];
-  
   return (
     <main className="flex flex-col h-screen bg-muted">
       <Card className="flex flex-col flex-1 max-w-2xl w-full mx-auto my-6 shadow-lg">
@@ -55,13 +54,13 @@ function ChatPage() {
           <div className="flex-1 space-y-2 overflow-y-auto">
             <ChatMessageList>
               {messages.map((message, index) => {
-                const variant = message.sender === "user" ? "sent" : "received";
+                const variant = message.sender === 'user' ? 'sent' : 'received';
                 return (
                   <ChatBubble key={message.id} variant={variant}>
                     {/* <ChatBubbleAvatar fallback={variant === "sent" ? "US" : "AI"} /> */}
                     <ChatBubbleMessage
                       isLoading={message.isLoading}
-                      className={message.sender === "user" ? "bg-sky-400" : ""}
+                      className={message.sender === 'user' ? 'bg-sky-400' : ''}
                     >
                       {message.message}
                     </ChatBubbleMessage>
@@ -74,7 +73,7 @@ function ChatPage() {
                           icon={<Icon className="size-4" />}
                           onClick={() =>
                             console.log(
-                              "Action " + type + " clicked for message " + index,
+                              'Action ' + type + ' clicked for message ' + index
                             )
                           }
                         />
