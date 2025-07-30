3a2bcb186eae1c919a7902e86b8db84a36e9261e
Megan Tegeder <megan.tegeder@gmail.com>
2025-07-30 17:07:31 -0500

    updating fields to be required

--- src/components/RegisterPage/steps/Step2.jsx
+++ src/components/RegisterPage/steps/Step2.jsx
@@ -6,10 +6,10 @@ function Step2({ data, onNext, onBack, onUpdate }) {
     <div className="space-y-4">
       <Button variant="ghost" onClick={onBack}>(arrow) Back</Button>
       <h2 className="text-2xl font-bold text-center">Create your profile</h2>
-      <Input placeholder="First Name" value={data.firstName} onChange={(e) => onUpdate({ firstName: e.target.value })} />
-      <Input placeholder="Last Name" value={data.lastName} onChange={(e) => onUpdate({ lastName: e.target.value })} />
-      <Input placeholder="Email" type="email" value={data.username} onChange={(e) => onUpdate({ username: e.target.value })} />
-      <Input placeholder="Password" type="password" value={data.password} onChange={(e) => onUpdate({ password: e.target.value })} />
+      <Input placeholder="First Name" required value={data.firstName} onChange={(e) => onUpdate({ firstName: e.target.value })} />
+      <Input placeholder="Last Name" required value={data.lastName} onChange={(e) => onUpdate({ lastName: e.target.value })} />
+      <Input placeholder="Email" required type="email" value={data.username} onChange={(e) => onUpdate({ username: e.target.value })} />
+      <Input placeholder="Password" required type="password" value={data.password} onChange={(e) => onUpdate({ password: e.target.value })} />
       <div className="flex justify-between">
         <Button onClick={onNext}>Continue</Button>
       </div>
