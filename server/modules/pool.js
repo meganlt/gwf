2bc28a05773b33800a856695a8f8d2a9b32a4ac2
Mase <mase@hackthegap.com>
2025-09-08 16:20:56 +0100

    Removed demo card from login page

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -19,7 +19,7 @@ if (process.env.DATABASE_URL) {
 else {
   pool = new pg.Pool({
     host: 'localhost',
-    port: 5432,
+    port: 5433,
     database: 'grow-with-flora',   // 👈 Change this to the name of your database!
   });
 }
