b8bfebb0add4f747bde2aa7ffb3f1ee8c9c9c3ee
Mase <mase@hackthegap.com>
2025-09-12 15:22:46 +0100

    Corrected port

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -19,7 +19,7 @@ if (process.env.DATABASE_URL) {
 else {
   pool = new pg.Pool({
     host: 'localhost',
-    port: 5433,
+    port: 5432,
     database: 'grow-with-flora',   // 👈 Change this to the name of your database!
   });
 }
